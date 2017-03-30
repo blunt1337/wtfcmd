a9e191d05e60bbeef83194c96796fdf6d521559a
Olivier Blunt <contact@blunt.sh>
2017-03-30 13:35:13 +0200

    Fix autcomplete tests with random ordered results

--- wtf/autocomplete_test.go
+++ wtf/autocomplete_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"os"
+	"sort"
 )
 
 // Sample config
@@ -24,20 +25,40 @@ func sampleConfig() []*Group {
 
 func Example1() {
 	groups := sampleConfig()
+	
+	res := autocomplete(groups, "act ", []string{"act"}, 4)
+	sort.Strings(res)
+	Made("autocomplete", res)
+	
+	res = autocomplete(groups, "act te", []string{"act", "te"}, 6)
+	sort.Strings(res)
+	Made("autocomplete", res)
+	
+	res = autocomplete(groups, "act forat", []string{"act", "forat"}, 7)
+	sort.Strings(res)
+	Made("autocomplete", res)
+	
+	res = autocomplete(groups, "act forat omg", []string{"act", "forat", "omg"}, 7)
+	sort.Strings(res)
+	Made("autocomplete", res)
+	
+	res = autocomplete(groups, "act      forat      omg", []string{"act", "forat", "omg"}, 11)
+	sort.Strings(res)
+	Made("autocomplete", res)
+	
+	res = autocomplete(groups, "act      \"forat\"      omg", []string{"act", "\"forat\"", "omg"}, 13)
+	sort.Strings(res)
+	Made("autocomplete", res)
+	
+	res = autocomplete(groups, "act  omg", []string{"act", "", "omg"}, 4)
+	sort.Strings(res)
+	Made("autocomplete", res)
 
-	Made("autocomplete", autocomplete(groups, "act ", []string{"act"}, 4))
-	Made("autocomplete", autocomplete(groups, "act te", []string{"act", "te"}, 6))
-	Made("autocomplete", autocomplete(groups, "act forat", []string{"act", "forat"}, 7))
-	Made("autocomplete", autocomplete(groups, "act forat omg", []string{"act", "forat", "omg"}, 7))
-	Made("autocomplete", autocomplete(groups, "act      forat      omg", []string{"act", "forat", "omg"}, 11))
-	Made("autocomplete", autocomplete(groups, "act      \"forat\"      omg", []string{"act", "\"forat\"", "omg"}, 13)) //TODO
-	Made("autocomplete", autocomplete(groups, "act  omg", []string{"act", "", "omg"}, 4))
-
-	// Output: [+] autocomplete [build test format]
+	// Output: [+] autocomplete [build format test]
 	// [+] autocomplete [test]
 	// [+] autocomplete [form]
 	// [+] autocomplete [form]
 	// [+] autocomplete [format]
 	// [+] autocomplete []
-	// [+] autocomplete [build test format]
+	// [+] autocomplete [build format test]
 }
